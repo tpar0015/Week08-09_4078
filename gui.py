40d36b80d395c44d8c033e9cf6c1969f4969a5ba
tpar0015 <tpar0015@student.monash.edu>
2023-10-03 12:00:25 +1100

    gui

--- gui.py
+++ gui.py
@@ -25,8 +25,7 @@ class GUI:
         self.pibot_pic = pygame.transform.rotate(pygame.image.load("pics/8bit/pibot_top.png"),180)
         self.state = [100, 100, 0]
         self.waypoints = []
-        # _, _, self.landmarks = read_true_map(map)
-        self.landmarks = [[0.0, 0.0], [.5, .5], [1, 1]]
+        _, _, self.landmarks = read_true_map(map)
         
         pygame.font.init()
         self.m2pixel = width / 3    # pixels / meter
@@ -92,12 +91,11 @@ class GUI:
         # Set background
         surface.blit(bg, (0, 0),special_flags=pygame.BLENDMODE_NONE)
         surface.blit(state_surf, (0, 0),special_flags=pygame.BLENDMODE_NONE)
-        surface.blit(landmark_surf, (0, 0),special_flags=pygame.BLEND_RGBA_ADD)
         surface.blit(waypoints_surf, (0, 0), special_flags=pygame.BLEND_RGBA_MAX)
+        surface.blit(landmark_surf, (0, 0),special_flags=pygame.BLEND_RGBA_ADD)
         
         # Mirror surface
         surface = pygame.transform.flip(surface, False, True)
-        surface = pygame.transform.rotate(surface, -90)
         
         self.screen.blit(surface, (0, 0))
         pygame.display.flip()
@@ -120,14 +118,14 @@ class GUI:
         surface.convert_alpha()
         surface.fill((0,0,0,0))
         for i in range(len(self.landmarks)):
-
             x,y = self.landmarks[i]
-            x = (x) * self.m2pixel + self.width/2
-            y = (y) * self.m2pixel  + self.height/2
+
+            x = x * self.m2pixel + self.width / 2
+            y = y * self.m2pixel + self.height / 2
             # Rotate images
             img = pygame.image.load(f"pics/8bit/lm_{i + 1}.png")
             img = pygame.transform.flip(img, False, True)
-            surface.blit(img, (x-15, y-15))
+            surface.blit(img, (x, y))
             # Label the landmarks
             # font = pygame.font.Font('freesansbold.ttf', 12)
             # text = font.render(str(i + 1) , True, (255, 255, 255), (120, 120, 120))
@@ -162,8 +160,7 @@ class GUI:
 
 
 if __name__=="__main__":
-    _, _, landmarks = read_true_map("M4_prac_map_full.txt")
-    gui = GUI(750, 750)
+    gui = GUI(750, 750, "map/M4_prac_map_full.txt")
     theta_range = np.linspace(0, 2*np.pi, 100)
     i_range = np.linspace(-3, 3, 100)
     j_range = np.linspace(-3, 3, 100)
